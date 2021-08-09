58ba78a0b723ba03e90d7184c0b4b6eb6ddfe2a8
ja <ivananl@yahoo.com>
2021-08-09 10:09:28 +0200

    Added state, render and handleClick to App component, passed value to display, passed handleClick to ButtonPanel

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,41 @@ import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
+import React from 'react';
 
-function App() {
-  const forLinters = Calculate('=', { total: '5', next: '3', operation: '+' });
-  return (
-    <>
-      <div>{forLinters}</div>
-      <Display />
-      <ButtonPanel />
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total = null,
+      next = null,
+      operation = null,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-    </>
-  );
+  const handleClick = (name) => {
+    const { total, next, operation} = this.state;
+    const data = { total, next, operation};
+    const calculation = Calculate(name, data);
+    }
+    this.setState({
+      total: calculation.total,
+      next: calculation.next,
+      operation: calculation.operation,
+      });
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <>
+
+        <Display calculation = {total || next}/>
+        <ButtonPanel clickHandler={this.handleClick} />
+
+      </>
+    );
+  }
 }
 
-export default App;
