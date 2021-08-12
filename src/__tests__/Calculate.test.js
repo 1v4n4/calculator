36dc7a2a918888a9d3372da9176352ad47b3e181
ja <ivananl@yahoo.com>
2021-08-12 15:19:44 +0200

    Add React Testing Library to handle tests

--- src/__tests__/Calculate.test.js
+++ src/__tests__/Calculate.test.js
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom/extend-expect';
 import Calculate from '../logic/calculate';
 
 describe('AC button', () => {
--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,5 +1,6 @@
 import renderer from 'react-test-renderer';
 import Calculator from '../components/Calculator';
+import '@testing-library/jest-dom/extend-expect';
 
 it('renders Calculator page correctly', () => {
   const calculator = renderer.create(<Calculator />).toJSON();
